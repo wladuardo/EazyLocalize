78b55ef471d20a3c271c78b7961e899a4196aac0
wladuardo <kovalskyvk@icloud.com>
2023-09-04 00:30:28 +0400

    Initial Commit

---
+++ EasyLocalize/EasyLocalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLocalizeApp.swift
+//  EasyLocalize
+//
+//  Created by Владислав Ковальский on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyLocalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
